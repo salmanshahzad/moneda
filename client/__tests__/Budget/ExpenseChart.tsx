67af0c0beee44c3f1da6dbe822510407dc4470b4
Salman Shahzad <salman@salshahzad.com>
2018-06-12 13:51:57 -0400

    Added ExpenseChart

---
+++ client/__tests__/Budget/ExpenseChart.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { mount } from "enzyme";
+import "../config";
+import ExpenseChart from "../../src/components/Budget/ExpenseChart";
+import { Pie } from "react-chartjs-2";
+
+describe("ExpenseChart", () => {
+    it("renders", () => {
+        const wrapper = mount(<ExpenseChart expenses={[]} />);
+        expect(wrapper.find(Pie)).toHaveLength(1);
+    });
+});
---
+++ client/src/components/Budget/ExpenseChart.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { Pie } from "react-chartjs-2";
+
+interface Expense {
+    name: string;
+    amount: number;
+    colour: string;
+}
+
+interface ExpenseChartProps {
+    expenses: Expense[];
+}
+
+export default class ExpenseChart extends React.Component<ExpenseChartProps, {}> {
+    getData = () => {
+        // return the data in the format needed for the Pie component
+        return {
+            labels: this.props.expenses.map(expense => expense.name),
+            datasets: [
+                {
+                    data: this.props.expenses.map(expense => expense.amount),
+                    backgroundColor: this.props.expenses.map(expense => expense.colour)
+                }
+            ]
+        };
+    };
+
+    options = {
+        tooltips: {
+            callbacks: {
+                label: (tooltipItem, data) => {
+                    const index = tooltipItem.index;
+                    const expense = data.labels[index];
+                    const amount = data.datasets[tooltipItem.datasetIndex].data[index];
+                    return `${expense}: $${amount.toFixed(2)}`;
+                }
+            }
+        }
+    };
+
+    render(): React.ReactNode {
+        return <Pie data={this.getData()} options={this.options} />;
+    }
+}
