2f7080905f39ce286c4691be8864e5211afbf91f
Salman Shahzad <salman@salshahzad.com>
2018-06-12 16:48:42 -0400

    Added colour for income and expenses

--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -1,47 +1,33 @@
 import * as mongoose from "mongoose";
+import { Expense, User as UserModel } from "../../../user";
 
-interface User {
-    username: string;
-    password: string;
-    income: Income[];
-    expenses: Expense[];
-}
+const generateRandomColour = (): string => {
+    const letters = "0123456789ABCDEF";
+    let colour = "#";
+    for (let i = 0; i < 6; i++) {
+        colour += letters[Math.floor(Math.random() * 16)];
+    }
+    return colour;
+};
 
-interface Income {
-    name: string;
-    income: number;
-    transactions: Transaction[];
-}
-
-interface Expense {
-    name: string;
-    spent: number;
-    budget: number;
-    transactions: Transaction[];
-}
-
-interface Transaction {
-    note: string;
-    date: number;
-    amount: number;
-}
-
-const createExpense = (name: string) => {
+const createExpense = (name: string): Expense => {
     return {
         name,
+        colour: generateRandomColour(),
         spent: 0,
         budget: 0,
         transactions: []
     };
 };
 
-export const createUser = (username: string, password: string): User => {
+export const createUser = (username: string, password: string): UserModel => {
     return {
         username,
         password,
         income: [
             {
                 name: "Primary Income",
+                colour: generateRandomColour(),
                 income: 0,
                 transactions: []
             }
