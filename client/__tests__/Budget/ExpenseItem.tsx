15e1476f8e2ae24a2b1f3401795d167003f69811
Salman Shahzad <salman@salshahzad.com>
2018-06-17 22:31:55 -0400

    Using ProgressBar component

--- client/__tests__/Budget/ExpenseItem.tsx
+++ client/__tests__/Budget/ExpenseItem.tsx
@@ -3,33 +3,21 @@ import { mount } from "enzyme";
 import routerContext from "../routerContext";
 import { Link } from "react-router-dom";
 import ExpenseItem from "../../src/components/Budget/ExpenseItem";
-import { Progress } from "semantic-ui-react";
+import ProgressBar from "../../src/components/Budget/ProgressBar";
 
 describe("ExpenseItem", () => {
     const wrapper = mount(<ExpenseItem name="Test" spent={1} budget={2} />, routerContext);
 
     it("renders", () => {
         expect(wrapper.find(Link)).toHaveLength(1);
-        expect(wrapper.find(Progress)).toHaveLength(1);
+        expect(wrapper.find(ProgressBar)).toHaveLength(1);
     });
 
     it("formats the information correctly", () => {
         expect(wrapper.find(Link).prop("to")).toBe("/expense/Test");
         expect(wrapper.find(Link).text()).toBe("Test");
         expect(wrapper.find("span").text()).toBe("$1.00 of $2.00");
-        expect(wrapper.find(Progress).prop("value")).toBe(1);
-        expect(wrapper.find(Progress).prop("total")).toBe(2);
-    });
-
-    it("has the correct colour for the Progress component", () => {
-        expect(wrapper.find(Progress).prop("color")).toBe("green");
-        wrapper.setProps({spent: 1.5});
-        expect(wrapper.find(Progress).prop("color")).toBe("yellow");
-        wrapper.setProps({spent: 2});
-        expect(wrapper.find(Progress).prop("color")).toBe("red");
-        wrapper.setProps({spent: 0, budget: 0});
-        expect(wrapper.find(Progress).prop("color")).toBe("green");
-        wrapper.setProps({spent: 1});
-        expect(wrapper.find(Progress).prop("color")).toBe("red");
+        expect(wrapper.find(ProgressBar).prop("value")).toBe(1);
+        expect(wrapper.find(ProgressBar).prop("total")).toBe(2);
     });
 });
--- client/src/components/Budget/ExpenseItem.tsx
+++ client/src/components/Budget/ExpenseItem.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { SemanticCOLORS, Progress } from "semantic-ui-react";
+import ProgressBar from "./ProgressBar";
 
 interface ExpenseItemProps {
     name: string;
@@ -13,31 +13,12 @@ const textStyle: React.CSSProperties = {
     fontWeight: "bold"
 };
 
-export default (props: ExpenseItemProps) => {
-    const progressColour = (): SemanticCOLORS => {
-        if (props.budget === 0) {
-            if (props.spent === 0) {
-                return "green";
-            } else {
-                return "red";
-            }
-        }
-        const percentage = props.spent / props.budget * 100;
-        if (percentage >= 100) {
-            return "red";
-        } else if (percentage < 75) {
-            return "green";
-        } else {
-            return "yellow";
-        }
-    };
-    return (
-        <React.Fragment>
-            <div style={{display: "flex", justifyContent: "space-between"}}>
-                <Link to={"/expense/" + props.name} style={textStyle}>{props.name}</Link>
-                <span style={textStyle}>${props.spent.toFixed(2)} of ${props.budget.toFixed(2)}</span>
-            </div>
-            <Progress value={props.spent} total={props.budget} progress="percent" color={progressColour()} style={{margin: "0.8em 0"}} />
-        </React.Fragment>
-    );
-}
+export default (props: ExpenseItemProps) => (
+    <React.Fragment>
+        <div style={{display: "flex", justifyContent: "space-between"}}>
+            <Link to={"/expense/" + props.name} style={textStyle}>{props.name}</Link>
+            <span style={textStyle}>${props.spent.toFixed(2)} of ${props.budget.toFixed(2)}</span>
+        </div>
+        <ProgressBar value={props.spent} total={props.budget} />
+    </React.Fragment>
+)
