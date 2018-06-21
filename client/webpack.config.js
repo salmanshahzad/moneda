20bb34449eae37dc39225adc2fba19f61e0d2b1c
Salman Shahzad <salman@salshahzad.com>
2018-06-20 22:02:51 -0400

    Added publicPath for assets

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -15,7 +15,8 @@ module.exports = env => ({
     entry: "./src/index.tsx",
     output: {
         filename: "bundle.js",
-        path: path.join(__dirname, "dist")
+        path: path.join(__dirname, "dist"),
+        publicPath: "/"
     },
     module: {
         rules: [
