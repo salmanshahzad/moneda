e50f6d41c7c376925d465ea5234a370377b8e6fc
Salman Shahzad <salman@salshahzad.com>
2018-06-23 16:10:54 -0400

    Added POST /api/update_user

--- server/__tests__/user.ts
+++ server/__tests__/user.ts
@@ -80,3 +80,59 @@ describe("/api/add_transaction", () => {
         }).expect(200, done);
     });
 });
+
+describe("/api/update_user", () => {
+    let server: request.SuperTest<request.Test>;
+
+    beforeAll(async done => {
+        server = request.agent(app);
+        await server.post("/api/sign_in").send({
+            username: "testuser2",
+            password: "testuser2"
+        });
+        done();
+    });
+
+    it("sends 401 when not signed in", done => {
+        request(app).post("/api/update_user").expect(401, done);
+    });
+
+    it("sends 400 with no inputs", done => {
+        server.post("/api/update_user").expect(400, [
+            "Please enter a username, password, confirm password, and current password."
+        ], done);
+    });
+
+    it("sends 400 with invalid inputs", done => {
+        server.post("/api/update_user").send({
+            username: "test",
+            password: "test",
+            confirmPassword: "test",
+            currentPassword: "test"
+        }).expect(400, [
+            "Please enter a username with at least 5 characters.",
+            "Please enter a password with at least 8 characters.",
+            "Incorrect password."
+        ], done);
+    });
+
+    it("sends 400 if the current password is incorrect", done => {
+        server.post("/api/update_user").send({
+            username: "testuser2",
+            password: "testuser22",
+            confirmPassword: "testuser22",
+            currentPassword: ""
+        }).expect(400, [
+            "Incorrect password."
+        ], done);
+    });
+
+    it("sends 200 with valid inputs", done => {
+        server.post("/api/update_user").send({
+            username: "testuser2",
+            password: "testuser22",
+            confirmPassword: "testuser22",
+            currentPassword: "testuser2"
+        }).expect(200, done);
+    });
+});
--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import db from "../../db";
+import bcrypt from "bcrypt";
 
 const router = express.Router();
 
@@ -67,6 +68,61 @@ router.post("/add_transaction", async (req, res) => {
         const spent = (await db("expenses").select("spent").where({user_id: userId, name: req.body.account}))[0].spent;
         await db("expenses").update({spent: spent + req.body.amount}).where({user_id: userId, name: req.body.account});
     }
+    res.sendStatus(200);
+});
+
+router.post("/update_user", async (req, res) => {
+    // ensure user is signed in
+    if (typeof req.session === "undefined" || typeof req.session.username === "undefined") {
+        return res.sendStatus(401);
+    }
+
+    // validate
+    if (typeof req.body.username === "undefined" || typeof req.body.password === "undefined" || typeof req.body.confirmPassword === "undefined" || typeof req.body.currentPassword === "undefined") {
+        return res.status(400).send(["Please enter a username, password, confirm password, and current password."]);
+    }
+    const errors = [];
+    // if username is not blank or username and not the same as before, check if already exists
+    if (req.body.username !== "" && req.body.username !== req.session.username) {
+        if (req.body.username.length < 5) {
+            errors.push("Please enter a username with at least 5 characters.");
+        } else {
+            const exists = (await db("users").select("username").where({username: req.body.username})).length > 0;
+            if (exists) {
+                errors.push("A user with that username already exits.");
+            }
+        }
+    }
+
+    if (req.body.password !== "") {
+        if (req.body.password.length < 8) {
+            errors.push("Please enter a password with at least 8 characters.");
+        } else if (req.body.confirmPassword !== req.body.password) {
+            errors.push("Passwords do not match.");
+        }
+    }
+
+    // check current password against hash
+    const hash = (await db("users").select("password").where({username: req.session.username}))[0].password;
+    const match = await bcrypt.compare(req.body.currentPassword, hash);
+    if (!match) {
+        errors.push("Incorrect password.");
+    }
+
+    if (errors.length > 0) {
+        return res.status(400).send(errors);
+    }
+
+    if (req.body.username !== "" && req.body.username !== req.session.username) {
+        await db("users").update({username: req.body.username}).where({username: req.session.username});
+        req.session.username = req.body.username;
+    }
+
+    if (req.body.password !== "") {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        await db("users").update({password: hashedPassword}).where({username: req.session.username});
+    }
+    
     res.sendStatus(200);
 });
 
