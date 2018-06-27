48c5b250d020d474356f139ff4a1203b944639c0
Salman Shahzad <salman@salshahzad.com>
2018-06-26 21:16:19 -0400

    Added POST /api/update_account and /api/delete_account

--- server/__tests__/user.ts
+++ server/__tests__/user.ts
@@ -130,9 +130,134 @@ describe("/api/update_user", () => {
     it("sends 200 with valid inputs", done => {
         server.post("/api/update_user").send({
             username: "testuser2",
-            password: "testuser22",
-            confirmPassword: "testuser22",
+            password: "testuser2",
+            confirmPassword: "testuser2",
             currentPassword: "testuser2"
+        }).expect(200, done);
+    });
+});
+
+describe("/api/update_account", () => {
+    let server: request.SuperTest<request.Test>;
+
+    beforeAll(async done => {
+        server = request.agent(app);
+        await server.post("/api/sign_in").send({
+            username: "testuser2",
+            password: "testuser2"
+        });
+        done();
+    });
+
+    it("sends 401 when not signed in", done => {
+        request(app).post("/api/update_account").expect(401, done);
+    });
+
+    it("sends 400 with no inputs", done => {
+        server.post("/api/update_account").expect(400, [
+            "Please enter income or expenses as the type.",
+            "Please enter a valid account id.",
+            "Please enter the new account name.",
+            "Please enter the new account colour as a hex string."
+        ], done);
+    });
+
+    it("sends 400 with invalid inputs", done => {
+        server.post("/api/update_account").send({
+            type: "test",
+            id: "test",
+            name: "test",
+            colour: "test"
+        }).expect(400, [
+            "Please enter income or expenses as the type.",
+            "Please enter a valid account id.",
+            "Please enter the new account colour as a hex string."
+        ], done);
+    });
+
+    it("sends 400 if the account does not exist", done => {
+        server.post("/api/update_account").send({
+            type: "income",
+            id: "d3aa88e2-c754-41e0-8ba6-4198a34aa0a2",
+            name: "Test",
+            colour: "#FF0000"
+        }).expect(400, [
+            "An account with that id does not exist."
+        ], done);
+    });
+
+    it("sends 400 if the type is expense and budget is not given", done => {
+        server.post("/api/update_account").send({
+            type: "expenses",
+            id: "d3aa88e2-c754-41e0-8ba6-4198a34aa0a2",
+            name: "Test",
+            colour: "#FF0000"
+        }).expect(400, [
+            "Please enter a non-negative budget."
+        ], done);
+    });
+
+    it("sends 200 with valid inputs", async done => {
+        const userId = (await db("users").select("id").where({username: "testuser2"}))[0].id;
+        const id = (await db("expenses").select("id").where({user_id: userId}))[0].id;
+        server.post("/api/update_account").send({
+            type: "expenses",
+            id,
+            name: "Test",
+            colour: "#FF0000",
+            budget: 1
+        }).expect(200, done);
+    });
+});
+
+describe("/api/delete_account", () => {
+    let server: request.SuperTest<request.Test>;
+
+    beforeAll(async done => {
+        server = request.agent(app);
+        await server.post("/api/sign_in").send({
+            username: "testuser2",
+            password: "testuser2"
+        });
+        done();
+    });
+
+    it("sends 401 when not signed in", done => {
+        request(app).post("/api/delete_account").expect(401, done);
+    });
+
+    it("sends 400 with no inputs", done => {
+        server.post("/api/delete_account").expect(400, [
+            "Please enter income or expenses as the type.",
+            "Please enter a valid account id."
+        ], done);
+    });
+
+    it("sends 400 with invalid inputs", done => {
+        server.post("/api/delete_account").send({
+            type: "test",
+            id: "test"
+        }).expect(400, [
+            "Please enter income or expenses as the type.",
+            "Please enter a valid account id."
+        ], done);
+    });
+
+    it("sends 400 if the account does not exist", done => {
+        server.post("/api/delete_account").send({
+            type: "income",
+            id: "d3aa88e2-c754-41e0-8ba6-4198a34aa0a2"
+        }).expect(400, [
+            "An account with that id does not exist."
+        ], done);
+    });
+
+    it("sends 200 with valid inputs", async done => {
+        const userId = (await db("users").select("id").where({username: "testuser2"}))[0].id;
+        const id = (await db("expenses").select("id").where({user_id: userId}))[0].id;
+        server.post("/api/delete_account").send({
+            type: "expenses",
+            id
         }).expect(200, done);
     });
 });
--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -113,4 +113,85 @@ router.post("/update_user", checkSignedIn, async (req, res) => {
     res.sendStatus(200);
 });
 
+router.post("/update_account", checkSignedIn, async (req, res) => {
+    // validate
+    const errors = [];
+    if (typeof req.body.type === "undefined" || (req.body.type !== "income" && req.body.type !== "expenses")) {
+        errors.push("Please enter income or expenses as the type.");
+    }
+
+    if (typeof req.body.id === "undefined" || !/[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}/.test(req.body.id)) {
+        errors.push("Please enter a valid account id.");
+    }
+    
+    if (typeof req.body.name === "undefined" || (typeof req.body.name === "string" && req.body.name.length === 0)) {
+        errors.push("Please enter the new account name.");
+    }
+
+    if (typeof req.body.colour === "undefined" || (typeof req.body.colour === "string" && req.body.colour.indexOf("#") !== 0)) {
+        errors.push("Please enter the new account colour as a hex string.");
+    }
+
+    // budget must be given if type is expenses
+    if (req.body.type === "expenses" && (typeof req.body.budget !== "number" || req.body.budget < 0)) {
+        errors.push("Please enter a non-negative budget.");
+    }
+
+    if (errors.length > 0) {
+        return res.status(400).send(errors);
+    }
+
+    // check that account with id exists
+    const accountExists = (await db(req.body.type).select().where({user_id: req.session.userId, id: req.body.id})).length > 0;
+    if (!accountExists) {
+        errors.push("An account with that id does not exist.");
+    }
+
+    const rows = await db(req.body.type).select("id", "name").where({user_id: req.session.userId, name: req.body.name});
+    if (rows.length > 0 && rows[0].id !== req.body.id && rows[0].name === req.body.name) {
+        errors.push("An account with that name already exists.");
+    }
+
+    if (errors.length > 0) {
+        return res.status(400).send(errors);
+    }
+
+    if (req.body.type === "income") {
+        await db("income").update({name: req.body.name, colour: req.body.colour.toUpperCase()}).where({user_id: req.session.userId, id: req.body.id});
+    } else {
+        await db("expenses").update({name: req.body.name, colour: req.body.colour.toUpperCase(), budget: req.body.budget}).where({user_id: req.session.userId, id: req.body.id});
+    }
+    res.sendStatus(200);
+});
+
+router.post("/delete_account", checkSignedIn, async (req, res) => {
+    // validate
+    const errors = [];
+    if (typeof req.body.type === "undefined" || (req.body.type !== "income" && req.body.type !== "expenses")) {
+        errors.push("Please enter income or expenses as the type.");
+    }
+
+    if (typeof req.body.id === "undefined" || !/[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}/.test(req.body.id)) {
+        errors.push("Please enter a valid account id.");
+    }
+
+    if (errors.length > 0) {
+        return res.status(400).send(errors);
+    }
+
+    // check that account with id exists
+    const accountExists = (await db(req.body.type).select().where({user_id: req.session.userId, id: req.body.id})).length > 0;
+    if (!accountExists) {
+        errors.push("An account with that id does not exist.");
+    }
+
+    if (errors.length > 0) {
+        return res.status(400).send(errors);
+    }
+
+    await db(req.body.type).delete().where({user_id: req.session.userId, id: req.body.id});
+    await db("transactions").delete().where({user_id: req.session.userId, account_id: req.body.id});
+    res.sendStatus(200);
+});
+
 export default router;
