42b637f9bf0c04a5c8a442e8adfb7dc6756be6f6
Salman Shahzad <salman@salshahzad.com>
2018-07-19 20:55:10 -0400

    Made ExpenseChart stateless

--- client/src/components/Budget/ExpenseChart.tsx
+++ client/src/components/Budget/ExpenseChart.tsx
@@ -6,40 +6,38 @@ interface ExpenseChartProps {
     expenses: Expense[];
 }
 
-export default class ExpenseChart extends React.Component<ExpenseChartProps, {}> {
-    getExpensesSum = (): number => {
+export default (props: ExpenseChartProps) => {
+    const getExpensesSum = (): number => {
         let sum = 0;
-        this.props.expenses.forEach(expense => sum += expense.spent);
+        props.expenses.forEach(expense => sum += expense.spent);
         return sum;
     }
 
-    getData = () => {
+    const getData = () => {
         // return the data in the format needed for the Pie component
         return {
-            labels: this.props.expenses.map(expense => expense.name),
+            labels: props.expenses.map(expense => expense.name),
             datasets: [
                 {
-                    data: this.props.expenses.map(expense => expense.spent),
-                    backgroundColor: this.props.expenses.map(expense => expense.colour)
+                    data: props.expenses.map(expense => expense.spent),
+                    backgroundColor: props.expenses.map(expense => expense.colour)
                 }
             ]
         };
     };
 
-    options = {
+    const options = {
         tooltips: {
             callbacks: {
                 label: (tooltipItem, data) => {
                     const index = tooltipItem.index;
                     const expense = data.labels[index];
                     const amount = data.datasets[tooltipItem.datasetIndex].data[index];
-                    return `${expense}: $${amount.toFixed(2)} (${(amount / this.getExpensesSum() * 100).toFixed(2)}%)`;
+                    return `${expense}: $${amount.toFixed(2)} (${(amount / getExpensesSum() * 100).toFixed(2)}%)`;
                 }
             }
         }
     };
 
-    render(): React.ReactNode {
-        return <Pie data={this.getData()} options={this.options} />;
-    }
+    return <Pie data={getData()} options={options} />;
 }
