d756e26a05ee75274129177c6ed7752720cb78a9
Salman Shahzad <salman@salshahzad.com>
2018-08-28 18:56:12 -0400

    Deleted checkSignedInMiddleware

--- server/src/checkSignedInMiddleware.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response, NextFunction } from "express";
-
-export default (req: Request, res: Response, next: NextFunction) => {
-    if (req.session === undefined || req.session.userId === undefined) {
-        res.sendStatus(401);
-    } else {
-        next();
-    }
-}
