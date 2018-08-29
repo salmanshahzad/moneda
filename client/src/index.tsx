d8f1f7781982a5b36d82b7e8ca9c4a30f9f94770
Salman Shahzad <salman@salshahzad.com>
2018-08-28 20:48:43 -0400

    Updated client-side routes

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import auth from "./auth";
 import { render } from "react-dom";
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import Home from "./components/Home/Home";
@@ -7,19 +6,21 @@ import Layout from "./components/General/Layout";
 import SignOut from "./components/General/SignOut";
 import "semantic-ui-css/semantic.min.css";
 
+const isAuthenticated = (): boolean => localStorage.getItem("token") !== null;
+
 const PrivateRoute = ({component: Component, ...rest}) => (
-    <Route render={props => auth.isAuthenticated() ? <Component {...props} /> : <Redirect to="/" />} {...rest} />
+    <Route render={props => isAuthenticated() ? <Component {...props} /> : <Redirect to="/" />} {...rest} />
 );
 
 render(
     <BrowserRouter basename={process.env.BASE}>
         <Switch>
-            <Route path="/" render={props => auth.isAuthenticated() ? <Redirect to="/dashboard" /> : <Home {...props} />} exact />
+            <Route path="/" render={props => isAuthenticated() ? <Redirect to="/dashboard" /> : <Home {...props} />} exact />
             <PrivateRoute path="/dashboard" component={Layout} exact />
             <PrivateRoute path="/budget" component={Layout} exact />
             <PrivateRoute path="/transactions" component={Layout} exact />
-            <PrivateRoute path="/income/:account" component={Layout} exact />
-            <PrivateRoute path="/expense/:account" component={Layout} exact />
+            <PrivateRoute path="/income/:name" component={Layout} exact />
+            <PrivateRoute path="/expense/:name" component={Layout} exact />
             <PrivateRoute path="/settings" component={Layout} exact />
             <Route path="/sign_out" component={SignOut} exact />
             <Route render={() => <Redirect to="/" />} />
