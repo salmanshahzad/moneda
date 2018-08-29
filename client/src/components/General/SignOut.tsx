c1f153189db2ee343c5743ae82f0b781eff6b706
Salman Shahzad <salman@salshahzad.com>
2018-08-28 20:57:58 -0400

    Updated SignOut to be stateless and remove token from localstorage

--- client/src/components/General/SignOut.tsx
+++ client/src/components/General/SignOut.tsx
@@ -1,22 +1,12 @@
 import React from "react";
-import auth from "../../auth";
 import { Redirect } from "react-router-dom";
 
-interface SignOutState {
-    done: boolean;
-}
-
-export default class SignOut extends React.Component<{}, SignOutState> {
-    state: SignOutState = {
-        done: false
-    };
-
-    async componentDidMount() {
-        await auth.signOut();
-        this.setState({done: true});
+export default class SignOut extends React.Component<{}, {}> {
+    componentDidMount() {
+        localStorage.removeItem("token");
     }
 
     render(): React.ReactNode {
-        return this.state.done ? <Redirect to="/" /> : <br />
+        return <Redirect to="/" />;
     }
 }
