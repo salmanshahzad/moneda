0687f5ad95ecd91976f4f455c8355f6ecaadba8d
Salman Shahzad <salman@salshahzad.com>
2018-08-29 17:26:02 -0400

    Added CategoryItem

---
+++ client/__tests__/Budget/CategoryItem.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { mount } from "enzyme";
+import CategoryItem from "../../src/components/Budget/CategoryItem";
+import testUser from "../testUser";
+import routerContext from "../routerContext";
+import { Link } from "react-router-dom";
+import ProgressBar from "../../src/components/Budget/ProgressBar";
+
+describe("CategoryItem", () => {
+    const wrapper = mount(<CategoryItem category={testUser.income[0]} />, routerContext);
+
+    it("shows income information correctly", () => {
+        expect(wrapper.find(Link).prop("to")).toBe("/income/Test Income 1");
+        expect(wrapper.find(Link).text()).toBe("Test Income 1");
+        expect(wrapper.find("span").text()).toBe("$0.00");
+        expect(wrapper.find(ProgressBar)).toHaveLength(0);
+    });
+
+    it("shows expense information correctly", () => {
+        wrapper.setProps({category: testUser.expenses[0]});
+        expect(wrapper.find(Link).prop("to")).toBe("/expense/Test Expense 1");
+        expect(wrapper.find(Link).text()).toBe("Test Expense 1");
+        expect(wrapper.find("span").text()).toBe("$0.00 of $0.00");
+        expect(wrapper.find(ProgressBar)).toHaveLength(1);
+    });
+});
---
+++ client/src/components/Budget/CategoryItem.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Category, Income, Expense } from "../../user";
+import { Link } from "react-router-dom";
+import ProgressBar from "./ProgressBar";
+
+interface CategoryItemProps {
+    category: Category;
+}
+
+export default (props: CategoryItemProps) => {
+    const textStyle: React.CSSProperties = {
+        fontStyle: "italic",
+        fontWeight: "bold"
+    };
+
+    return (
+        <React.Fragment>
+            <div style={{display: "flex", justifyContent: "space-between"}}>
+                <Link to={`/${props.category.type}/${props.category.name}`} style={textStyle}>{props.category.name}</Link>
+                {
+                    props.category.type === "income" &&
+                    <span style={textStyle}>${(props.category as Income).income.toFixed(2)}</span>
+                }
+                {
+                    props.category.type === "expense" &&
+                    <span style={textStyle}>${(props.category as Expense).spent.toFixed(2)} of ${(props.category as Expense).budget.toFixed(2)}</span>
+                }
+            </div>
+            {
+                props.category.type === "expense" &&
+                <ProgressBar value={(props.category as Expense).spent} total={(props.category as Expense).budget} />
+            }
+        </React.Fragment>
+    );
+}
