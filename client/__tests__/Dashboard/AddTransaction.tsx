164937bf7257bdf9be927fcc93a4db73097142ec
Salman Shahzad <salman@salshahzad.com>
2018-08-28 22:36:34 -0400

    Combined AddIncome/ExpenseTransaction to AddTransaction

--- client/__tests__/Dashboard/AddTransaction.tsx
+++ client/__tests__/Dashboard/AddTransaction.tsx
@@ -1,12 +1,48 @@
 import React from "react";
 import { mount } from "enzyme";
 import AddTransaction from "../../src/components/Dashboard/AddTransaction";
-import { Tab } from "semantic-ui-react";
+import testUser from "../testUser";
+import { Form, Message } from "semantic-ui-react";
+import moment from "moment";
 
 describe("AddTransaction", () => {
+    const wrapper = mount(<AddTransaction categories={testUser.expenses} onAddTransaction={jest.fn(() => new Promise<{}>((resolve, reject) => {}))} />);
+
     it("renders", () => {
-        const emptyPromise = jest.fn(() => new Promise<{}>((resolve, reject) => {}));
-        const wrapper = mount(<AddTransaction incomeNames={[]} onAddIncomeTransaction={emptyPromise} expenseNames={[]} onAddExpenseTransaction={emptyPromise} />);
-        expect(wrapper.find(Tab)).toHaveLength(1);
+        expect(wrapper.find(Form)).toHaveLength(1);
+    });
+
+    it("has the correct initial state", () => {
+        expect(wrapper.state("categoryId")).toBe("2");
+        expect(wrapper.state("amount")).toBe("0.00");
+        expect(wrapper.state("note")).toBe("");
+        expect(wrapper.state("date")).toEqual(moment().startOf("day"));
+        expect(wrapper.state("errors")).toEqual([]);
+    });
+
+    it("changes the state when an input value changes", () => {
+        // cannot test Select onChange because Select does not render a HTML select element
+        // wrapper.find(Select).simulate("change", {target: {value: "Food"}});
+        // expect(wrapper.state("name")).toBe("Food");
+        wrapper.find("input").at(0).simulate("change", {target: {value: "5.00"}});
+        expect(wrapper.state("amount")).toBe("5.00");
+        wrapper.find("input").at(1).simulate("change", {target: {value: "Test"}});
+        expect(wrapper.state("note")).toBe("Test");
+    });
+
+    it("displays errors", () => {
+        expect(wrapper.find(Message.Item)).toHaveLength(0);
+        wrapper.setState({errors: ["test error 1", "test error 2"]});
+        expect(wrapper.find(Message.Item)).toHaveLength(2);
+        expect(wrapper.find(Message.Item).at(0).text()).toBe("test error 1");
+        expect(wrapper.find(Message.Item).at(1).text()).toBe("test error 2");
+        wrapper.setState({errors: []});
+        expect(wrapper.find(Message.Item)).toHaveLength(0);
+    });
+
+    it("calls onAddTransaction when the form is submitted", () => {
+        wrapper.setState({categoryId: "3", amount: "5.00", note: "Test"});
+        wrapper.find(Form).simulate("submit");
+        expect(wrapper.prop("onAddTransaction")).toHaveBeenCalledWith("3", 5, "Test", moment().startOf("day").valueOf());
     });
 });
--- client/src/components/Dashboard/AddTransaction.tsx
+++ client/src/components/Dashboard/AddTransaction.tsx
@@ -1,25 +1,106 @@
 import React from "react";
-import AddIncomeTransaction from "./AddIncomeTransaction";
-import AddExpenseTransaction from "./AddExpenseTransaction";
-import { Tab } from "semantic-ui-react";
+import { Category } from "../../user";
+import { Form, Message, Input, Select, Button, DropdownProps, InputOnChangeData } from "semantic-ui-react";
+import moment from "moment";
+import "react-datepicker/dist/react-datepicker.css";
+import DatePicker from "react-datepicker";
 
 interface AddTransactionProps {
-    incomeNames: string[];
-    onAddIncomeTransaction: (name: string, amount: number, note: string, date: number) => Promise<{}>;
-    expenseNames: string[];
-    onAddExpenseTransaction: (name: string, amount: number, note: string, date: number) => Promise<{}>;
+    categories: Category[];
+    onAddTransaction: (categoryId: string, amount: number, note: string, date: number) => Promise<{}>;
 }
 
-export default (props: AddTransactionProps) => {
-    const panes = [
-        {
-            menuItem: "Expense",
-            render: () => <AddExpenseTransaction expenseNames={props.expenseNames} onAddExpenseTransaction={props.onAddExpenseTransaction} />
-        },
-        {
-            menuItem: "Income",
-            render: () => <AddIncomeTransaction incomeNames={props.incomeNames} onAddIncomeTransaction={props.onAddIncomeTransaction} />
+interface AddTransactionState {
+    categoryId: string;
+    amount: string;
+    note: string;
+    date: moment.Moment;
+    errors: string[];
+}
+
+export default class AddTransaction extends React.Component<AddTransactionProps, AddTransactionState> {
+    state: AddTransactionState = {
+        categoryId: this.props.categories[0].id,
+        amount: "0.00",
+        note: "",
+        date: moment().startOf("day"),
+        errors: []
+    };
+
+    getOptions = () => {
+        // returns expense names in the format needed for the Select component
+        return this.props.categories.map((category, i) => {
+            return {
+                key: i,
+                value: category.id,
+                text: category.name
+            };
+        });
+    };
+
+    changeName = (e: React.SyntheticEvent<HTMLElement>, data: DropdownProps) => {
+        this.setState({categoryId: data.value as string});
+    };
+
+    changeAmount = (e: React.SyntheticEvent<HTMLElement>, data: InputOnChangeData) => {
+        this.setState({amount: data.value});
+    };
+
+    makeAmountTwoDecimalPlaces = () => {
+        this.setState({amount: parseFloat(this.state.amount).toFixed(2)});
+    };
+
+    changeNote = (e: React.SyntheticEvent<HTMLElement>, data: InputOnChangeData) => {
+        this.setState({note: data.value});
+    };
+
+    changeDate = (date: moment.Moment, e: React.SyntheticEvent<any>) => {
+        this.setState({date});
+    };
+
+    submit = async () => {
+        try {
+            const date = this.state.date || moment().startOf("day");
+            await this.props.onAddTransaction(this.state.categoryId, parseFloat(this.state.amount), this.state.note, date.valueOf());
+            this.setState({
+                categoryId: this.props.categories[0].id,
+                amount: "0.00",
+                note: "",
+                date: moment().startOf("day"),
+                errors: []
+            });
+        } catch (errors) {
+            this.setState({errors});
         }
-    ];
-    return <Tab panes={panes} />;
+    };
+
+    render(): React.ReactNode {
+        return (
+            <Form onSubmit={this.submit} error={this.state.errors.length > 0} style={{paddingTop: "1rem"}}>
+                <Message error>
+                    <Message.Header>Error</Message.Header>
+                    <Message.List>
+                        {
+                            this.state.errors.map((error, i) => (
+                                <Message.Item key={i}>{error}</Message.Item>
+                            ))
+                        }
+                    </Message.List>
+                </Message>
+                <Form.Field>
+                    <Select options={this.getOptions()} value={this.state.categoryId} onChange={this.changeName} fluid />
+                </Form.Field>
+                <Form.Field>
+                    <Input type="number" min="0" step="0.01" label="$" value={this.state.amount} onChange={this.changeAmount} onBlur={this.makeAmountTwoDecimalPlaces} fluid />
+                </Form.Field>
+                <Form.Field>
+                    <Input type="text" placeholder="Note (optional)" value={this.state.note} onChange={this.changeNote} fluid />
+                </Form.Field>
+                <Form.Field>
+                    <DatePicker selected={this.state.date} onChange={this.changeDate} dateFormat="MMMM DD, YYYY" todayButton="Today" />
+                </Form.Field>
+                <Button primary type="submit">Add</Button>
+            </Form>
+        );
+    }
 }
