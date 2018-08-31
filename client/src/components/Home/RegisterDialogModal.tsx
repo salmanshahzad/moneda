4a6294d2978abfc33830fd9e0384d146456ee225
Salman Shahzad <salman@salshahzad.com>
2018-08-30 20:59:34 -0400

    Refactored code

--- client/src/components/Home/RegisterDialogModal.tsx
+++ client/src/components/Home/RegisterDialogModal.tsx
@@ -8,16 +8,10 @@ interface RegisterDialogModalProps {
     onRegister: (username: string, password: string, confirmPassword: string) => Promise<{}>;
 }
 
-export default (props: RegisterDialogModalProps): JSX.Element => {
-    const close = () => {
-        props.onClose();
-    };
-
-    // RegisterDialog is a separate component inside the Modal because Modals cannot be tested; see https://github.com/Semantic-Org/Semantic-UI-React/issues/1518
-    return (
-        <Modal size="mini" open={props.open} onClose={close}>
-            <Header>Register</Header>
-            <RegisterDialog onRegister={props.onRegister} />
-        </Modal>
-    );
-}
+// RegisterDialog is a separate component inside the Modal because Modals cannot be tested; see https://github.com/Semantic-Org/Semantic-UI-React/issues/1518
+export default (props: RegisterDialogModalProps): JSX.Element => (
+    <Modal size="mini" open={props.open} onClose={props.onClose}>
+        <Header>Register</Header>
+        <RegisterDialog onRegister={props.onRegister} />
+    </Modal>
+);
--- client/src/components/Home/SignInDialogModal.tsx
+++ client/src/components/Home/SignInDialogModal.tsx
@@ -8,16 +8,10 @@ interface SignInDialogModalProps {
     onSignIn: (username: string, password: string) => Promise<{}>;
 }
 
-export default (props: SignInDialogModalProps): JSX.Element => {
-    const close = () => {
-        props.onClose();
-    };
-
-    // SignInDialog is a separate component inside the Modal because Modals cannot be tested; see https://github.com/Semantic-Org/Semantic-UI-React/issues/1518
-    return (
-        <Modal size="mini" open={props.open} onClose={close}>
-            <Header>Sign In</Header>
-            <SignInDialog onSignIn={props.onSignIn} />
-        </Modal>
-    );
-}
+// SignInDialog is a separate component inside the Modal because Modals cannot be tested; see https://github.com/Semantic-Org/Semantic-UI-React/issues/1518
+export default (props: SignInDialogModalProps): JSX.Element => (
+    <Modal size="mini" open={props.open} onClose={props.onClose}>
+        <Header>Sign In</Header>
+        <SignInDialog onSignIn={props.onSignIn} />
+    </Modal>
+);
