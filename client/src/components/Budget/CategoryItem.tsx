728dc3d6d8813b7b774947869a012b54656de35d
Salman Shahzad <salman@salshahzad.com>
2018-08-30 21:00:25 -0400

    Moved textStyle outside of function

--- client/src/components/Budget/CategoryItem.tsx
+++ client/src/components/Budget/CategoryItem.tsx
@@ -7,29 +7,27 @@ interface CategoryItemProps {
     category: Category;
 }
 
-export default (props: CategoryItemProps): JSX.Element => {
-    const textStyle: React.CSSProperties = {
-        fontStyle: "italic",
-        fontWeight: "bold"
-    };
+const textStyle: React.CSSProperties = {
+    fontStyle: "italic",
+    fontWeight: "bold"
+};
 
-    return (
-        <React.Fragment>
-            <div style={{ display: "flex", justifyContent: "space-between" }}>
-                <Link to={`/${props.category.type}/${props.category.name}`} style={textStyle}>{props.category.name}</Link>
-                {
-                    props.category.type === "income" &&
-                    <span style={textStyle}>${(props.category as Income).income.toFixed(2)}</span>
-                }
-                {
-                    props.category.type === "expense" &&
-                    <span style={textStyle}>${(props.category as Expense).spent.toFixed(2)} of ${(props.category as Expense).budget.toFixed(2)}</span>
-                }
-            </div>
+export default (props: CategoryItemProps): JSX.Element => (
+    <React.Fragment>
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <Link to={`/${props.category.type}/${props.category.name}`} style={textStyle}>{props.category.name}</Link>
+            {
+                props.category.type === "income" &&
+                <span style={textStyle}>${(props.category as Income).income.toFixed(2)}</span>
+            }
             {
                 props.category.type === "expense" &&
-                <ProgressBar value={(props.category as Expense).spent} total={(props.category as Expense).budget} />
+                <span style={textStyle}>${(props.category as Expense).spent.toFixed(2)} of ${(props.category as Expense).budget.toFixed(2)}</span>
             }
-        </React.Fragment>
-    );
-}
+        </div>
+        {
+            props.category.type === "expense" &&
+            <ProgressBar value={(props.category as Expense).spent} total={(props.category as Expense).budget} />
+        }
+    </React.Fragment>
+);
