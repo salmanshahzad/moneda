4b973169bd11dbdc65be8aaa245fbc89e6bdd506
Salman Shahzad <salman@salshahzad.com>
2018-09-05 14:22:12 -0400

    Added console log after listening

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,7 @@
-import app from "./app";
 import { config } from "dotenv";
 import { join } from "path";
+import app from "./app";
 
 config({ path: join(__dirname, "..", "..", ".env") });
 
-app.listen(process.env.PORT);
+app.listen(process.env.PORT, () => console.log(`Moneda listening on http://localhost:${process.env.PORT}`));
